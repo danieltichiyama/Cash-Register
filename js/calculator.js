e9788aada89b3e872160abeb7c930f26a3f3f31e
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-06-24 21:53:39 -1000

    need to add a way to make the results of an equal to come out with commas

--- js/calculator.js
+++ js/calculator.js
@@ -1,113 +1,111 @@
 var calculatorModule = function() {
-    var memory=0;
-    var total=0;
+    var memory=[];
+    var total;
     var register = 0;
-    var arr = [];
 
     function getRegister (){
+        console.log("getRegister:register",register);
         return register;
     }
 
     function load(x){
-      arr[arr.length] = x;
-      memory = parseFloat(arr.join(""));
-      console.log("load:arr",arr, "load:memory",memory)
-      return parseFloat(arr.join(""));
-    }
+      console.log(x);
+      if (parseFloat(x) !== "number"){
+          memory[memory.length] = x;
+          console.log("load:memory",memory);
+          return memory[memory.length-1];
+      }
+      memory[memory.length] = parseFloat(x);
+      console.log("load:memory",memory);
+      return memory[memory.length];
+    };
 
     function clear(){
-        arr = [];
         total = 0;
-        memory = 0;
-        return arr;
-    }
-
-    function addDeci (){
-        if (arr.indexOf(".")===-1){
-            arr.push(".");
-        }
-        return arr.join("");
-    }
-
-    function equal(){
-       return total;
-    }
-
-    function add(x) {
-        console.log ("add:x", x);
-        console.log("add:total", total);
-        total += x;
-        arr = [];
-        console.log('add:memory ',memory,'add:total ',total,"add:arr ", arr)
+        memory = [];
         return total;
     }
 
-    function subtract(x){
-       this.validation(x);
-       total -= x;
-       return total;
-    }
-
-    function multiply(x){
-       this.validation(x);
-       total *= x;
-       return total;
-    }
-
-    function divide(x){
-       this.validation(x);
-       total /= x;
-       return total;
-    }
-
-    function recallMemory(){
-        return memory;
-    }
-
-    function saveMemory(){
-        if (typeof total !== "number"){
-        return "ERR";
+    function calculate (){
+        let i = 0;
+        // debugger;
+        while(memory.length>2){
+            if(typeof memory[i] ==="number"){
+                memory[i]=memory[i];
+            }else if(memory[i]==="+"){
+                var res = add(memory[i-1],memory[i+1]); 
+                memory.splice(0,3,res);
+                total = res;
+                console.log("calculate:memory",memory,"calculate:total",total)
+            }else if(memory[i]==="-"){
+                var res = subtract(memory[i-1],memory[i+1]); 
+                memory.splice(0,3,res);
+                total = res;
+                console.log("calculate:memory",memory,"calculate:total",total)
+            }else if(memory[i]==="x"){
+                var res = multiply(memory[i-1],memory[i+1]); 
+                memory.splice(0,3,res);
+                total = res;
+                console.log("calculate:memory",memory,"calculate:total",total)
+            }else if(memory[i]==="/"){
+                var res = divide(memory[i-1],memory[i+1]); 
+                memory.splice(0,3,res);
+                total = res;
+                console.log("calculate:memory",memory,"calculate:total",total)
+            };
+            i++;
         }
-        memory = total;
-        return memory;
-    }
-
-    function clearMemory(){
-        memory = 0;
-        return memory;
-    }
-
-    function deposit(){
-        register +=total;
-        return register;
-    }
-
-    function withdrawal(){
-        register-=total;
-        return register;
-    }
-
-    function validation(){
-        if (typeof x !=='number'){
-            return 'Error';
+        if (memory[memory.length-1]==="="){
+            memory = [];
+            console.log("memory after equals",memory);
         }
+        return total;
+    }
+
+    function add(x,y) {
+        sum = x+y;
+        return sum;
+    }
+
+    function subtract(x,y){
+        diff = x-y;
+        return diff;
+    }
+
+    function multiply(x,y){
+       prod = x*y;
+       return prod;
+    }
+
+    function divide(x,y){
+       quot = x/y;
+       return quot;
+    }
+
+    function deposit(x){
+        register +=x;
+        console.log(x);
+        console.log("deposit:register",register);
+        return x;
+    }
+
+    function withdraw(x){
+        register-=x;
+        console.log(x);
+        console.log("withdraw:register",register);
+        return x;
     }
 
  return {
      getRegister: getRegister,
      load: load,
      clear: clear,
-     addDeci: addDeci,
-     equal: equal,
+     calculate: calculate,
      add: add,
      subtract: subtract,
      multiply: multiply,
      divide: divide,
-     recallMemory: recallMemory,
-     saveMemory: saveMemory,
-     clearMemory: clearMemory,
      deposit:deposit,
-     withdrawal:withdrawal,
-     validation: validation
+     withdraw:withdraw,
  };
 };
--- js/cash_register.js
+++ js/cash_register.js
@@ -7,17 +7,34 @@
 
 var calc = calculatorModule();
 
-var numberButtons = document.querySelectorAll (".number")
+var numberButtons = document.querySelectorAll (".number");
+var functionButtons = document.querySelectorAll(".function");
+var registerButtons = document.querySelectorAll(".register");
+var display = document.querySelector("#display");
+var str = "";
 
-function putDisplay (func){
-    var display = document.querySelector ("#display");
-    display.value = numberWithCommas(func);
+function putDisplay (){
+    str = str.concat(this.value);
+    display.value = numberWithCommas(str);
 }
 
 function numberWithCommas(x) {
-    var parts = x.toString().split(".");
+    var parts = x.split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
+}
+
+function numberWithoutCommas(x){
+    var parts = x.split(".");
+    parts[0] = parts[0].replace(/,/g, "");
+    return parts.join(".");
+
+}
+
+function addWipe (){
+    for(i=0;i<numberButtons.length;i++){
+        numberButtons[i].addEventListener('click', wipeDisplay);
+    }
 }
 
 function wipeDisplay (){
@@ -25,38 +42,43 @@ function wipeDisplay (){
     for (i=0;i<numberButtons.length;i++){
     numberButtons[i].removeEventListener('click',wipeDisplay);
     };
-    display.value=this.value;
+    str=this.value;
+    display.value = numberWithCommas(str);
 }
 
 for (i=0;i<numberButtons.length;i++){
-    numberButtons[i].addEventListener('click', function(){
-        let num = calc.load(this.value);
-        putDisplay(num);
+    numberButtons[i].addEventListener('click', putDisplay);
+}
+
+for (i=0;i<functionButtons.length;i++){
+    functionButtons[i].addEventListener('click', function (){
+        calc.load(parseFloat(numberWithoutCommas(display.value)));
+        calc.load(this.value);
+        if (typeof calc.calculate()==="number"){
+        display.value = calc.calculate();
+        }
+        addWipe();
     });
 }
 
-document.querySelector("#clear").addEventListener('click', function() {
+for (i = 0;i<registerButtons.length;i++){
+    registerButtons[i].addEventListener('click',function(){
+        if (this.value ==="deposit"){
+            display.value = calc.deposit (parseFloat(numberWithoutCommas(display.value)));
+        }else if(this.value ==="withdraw"){
+            display.value = calc.withdraw (parseFloat(numberWithoutCommas(display.value)));
+        }else if (this.value ==="balance"){
+            display.value = calc.getRegister();
+        }
+        addWipe();
+    })
+}
+document.querySelector("#clear").addEventListener('click',function(){
+    display.value = "CLEARED";
     calc.clear();
-    putDisplay(calc.clear());
+    addWipe;
 });
 
-document.querySelector("#balance").addEventListener('click',function(){
-    calc.getRegister();
-    putDisplay(calc.getRegister());
-})
-
-document.querySelector("#deci").addEventListener('click',function(){
-    let num = calc.addDeci();
-    putDisplay(num);
-})
-
-document.querySelector("#plus").addEventListener('click',function(){
-    let num = calc.add(calc.recallMemory());
-    putDisplay(num);
-    for(i=0;i<numberButtons.length;i++){
-        numberButtons[i].addEventListener('click', wipeDisplay);
-    }
-    
-});
+document.querySelector
 
 })();
